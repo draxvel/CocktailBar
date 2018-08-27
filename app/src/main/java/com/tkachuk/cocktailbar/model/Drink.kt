c020596d54bd3f755e0811ef6f7071505e0662b3
draxvel <ntv3331998@gmail.com>
2018-08-27 12:10:02 +0300

    Added model for drink.

---
+++ app/src/main/java/com/tkachuk/cocktailbar/model/Drink.kt
@@ -0,0 +1,6 @@
+package com.tkachuk.cocktailbar.model
+
+data class Drink(val idDrink: Int, val strDrink: String, val strCategory: String,
+                 val strAlcoholic: String, val strInstructions: String, val strDrinkThumb: String,
+                 val strIngredients: String, val strMeasures: String) {
+}
