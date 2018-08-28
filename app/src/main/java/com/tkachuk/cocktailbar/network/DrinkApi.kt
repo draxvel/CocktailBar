3f3c5b0a54c4d9db95f2a146cc7d351be7365d41
draxvel <ntv3331998@gmail.com>
2018-08-28 17:57:06 +0300

    Updated Api.

--- app/src/main/java/com/tkachuk/cocktailbar/network/DrinkApi.kt
+++ app/src/main/java/com/tkachuk/cocktailbar/network/DrinkApi.kt
@@ -1,11 +1,11 @@
 package com.tkachuk.cocktailbar.network
 
-import com.tkachuk.cocktailbar.model.Drink
+import com.tkachuk.cocktailbar.model.Ingredients
 import io.reactivex.Observable
 import retrofit2.http.GET
 
 interface DrinkApi {
 
-    @GET("/list.php?i=list")
-    fun getIngredientsList(): Observable<Drink>
+    @GET("list.php?i=list")
+    fun getIngredientsList(): Observable<Ingredients>
 }
