a3b6115184a69c8abb8f0fa107e8a525149caa1b
draxvel <ntv3331998@gmail.com>
2018-08-28 17:56:46 +0300

    Updated model classes.

--- app/src/main/java/com/tkachuk/cocktailbar/model/Drink.kt
+++ app/src/main/java/com/tkachuk/cocktailbar/model/Drink.kt
@@ -2,5 +2,4 @@ package com.tkachuk.cocktailbar.model
 
 data class Drink(val idDrink: Int, val strDrink: String, val strCategory: String,
                  val strAlcoholic: String, val strInstructions: String, val strDrinkThumb: String,
-                 val strIngredients: String, val strMeasures: String) {
-}
+                 val strIngredients: String, val strMeasures: String)
