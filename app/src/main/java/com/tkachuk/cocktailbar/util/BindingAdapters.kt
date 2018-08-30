8f857e944feaac8825125b9e3f173a12d2577966
draxvel <ntv3331998@gmail.com>
2018-08-30 15:20:44 +0300

    Added method to binding adapter.

--- app/src/main/java/com/tkachuk/cocktailbar/util/BindingAdapters.kt
+++ app/src/main/java/com/tkachuk/cocktailbar/util/BindingAdapters.kt
@@ -5,7 +5,9 @@ import android.arch.lifecycle.Observer
 import android.databinding.BindingAdapter
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.tkachuk.cocktailbar.util.extension.getParentActivity
 
 @BindingAdapter("mutableVisibility")
@@ -19,8 +21,20 @@ fun setMutableVisibility(view: View, visibility: MutableLiveData<Int>?) {
 @BindingAdapter("mutableText")
 fun setMutableText(view: TextView, text: MutableLiveData<String>?) {
     val parentActivity = view.getParentActivity()
-    if(parentActivity != null && text != null) {
-        text.observe(parentActivity, Observer { value -> view.text = value?:""})
+    if (parentActivity != null && text != null) {
+        text.observe(parentActivity, Observer { value -> view.text = value ?: "" })
+    }
+}
+
+@BindingAdapter("mutableImage")
+fun setMutableImage(view: ImageView, url: MutableLiveData<String>?) {
+    val parentActivity = view.getParentActivity()
+    if (parentActivity != null && url != null) {
+        url.observe(parentActivity, Observer { value ->
+            Glide.with(parentActivity)
+                    .load(value)
+                    .into(view)
+        })
     }
 }
 
