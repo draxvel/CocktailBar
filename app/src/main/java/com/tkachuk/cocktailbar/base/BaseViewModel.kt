4f03268fd485156ff90b4056ca9a80bdf58b216a
draxvel <ntv3331998@gmail.com>
2018-09-07 11:22:36 +0300

    Added injector for CategoryListViewModel.

--- app/src/main/java/com/tkachuk/cocktailbar/base/BaseViewModel.kt
+++ app/src/main/java/com/tkachuk/cocktailbar/base/BaseViewModel.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModel
 import com.tkachuk.cocktailbar.injection.component.DaggerViewModelInjector
 import com.tkachuk.cocktailbar.injection.component.ViewModelInjector
 import com.tkachuk.cocktailbar.injection.module.NetworkModule
+import com.tkachuk.cocktailbar.ui.categories.CategoryListViewModel
 import com.tkachuk.cocktailbar.ui.drinks.DrinkListViewModel
 import com.tkachuk.cocktailbar.ui.fulldrink.FullDrinkViewModel
 import com.tkachuk.cocktailbar.ui.ingredients.IngredientsListViewModel
@@ -26,6 +27,7 @@ abstract class BaseViewModel : ViewModel() {
             is IngredientsListViewModel -> injector.inject(this)
             is DrinkListViewModel -> injector.inject(this)
             is FullDrinkViewModel -> injector.inject(this)
+            is CategoryListViewModel -> injector.inject(this)
         }
     }
 }
